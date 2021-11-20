546fe3289f953d14ae9088133e6f642e96e1c1c7
Ajay Fewell <afewell2@illinois.edu>
2021-11-20 13:06:02 -0600

    just reorganized code - moved javascrpit to index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-/*
+let flightPath;
 
-var fr = new FileReader();
-fr.readAsText("./data_parsed.csv");
-fr.onload = function () {
-    console.log(fr.result)
-};
+let threshold = 5000;
 
-*/
+// want dict[src][dst] = 
+// 
 
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: { lat: 0, lng: -180 },
+        mapTypeId: "terrain",
+    });
+    const flightPlanCoordinates = [
+        { lat: 37.772, lng: -122.214 },
+        { lat: 21.291, lng: -157.821 },
+        { lat: -18.142, lng: 178.431 },
+        { lat: -27.467, lng: 153.027 },
+    ];
+    flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
+    flightPath.setMap(map);
+}
 
-console.log(data["1990"])
+initMap();
